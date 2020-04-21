04cd52ed76cdfdca2f5b35967459fd4def538344
Saj King <a.sajking@gmail.com>
2020-04-21 17:25:10 -0400

    added routes for single user and product by id

--- server/api/users.js
+++ server/api/users.js
@@ -5,12 +5,23 @@ module.exports = router
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
       attributes: ['id', 'firstName', 'lastName', 'email', 'isAdmin']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// Find out where logged in user is stored for isAdmin function!!
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    const user = await User.findByPk(id, {
+      attributes: ['id', 'firstName', 'lastName', 'email', 'isAdmin']
+    })
+    res.json(user)
   } catch (err) {
     next(err)
   }
