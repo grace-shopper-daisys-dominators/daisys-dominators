2bc3bc21f46f35197fb36554b41ba26c6f194760
Angie Spears <angiespears@chazs-mbp.lan>
2020-04-22 16:03:15 -0400

    create reducer for single product

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import singleProduct from './singleProduct'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, singleProduct})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
