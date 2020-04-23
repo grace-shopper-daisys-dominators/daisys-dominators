12c7c792a5edd5e86385ba8a959dd761b4dff15b
Saj King <a.sajking@gmail.com>
2020-04-23 19:06:44 -0400

    added cart routes post and put

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Order, User, Cart, Product} = require('../db/models')
+const {Order, Product} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
