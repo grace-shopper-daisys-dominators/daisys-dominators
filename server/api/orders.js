0817cb7f632b5e37e5df84b937f5d4274dbc5e83
Saj King <a.sajking@gmail.com>
2020-04-23 15:39:03 -0400

    added put route for orders

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Order, User} = require('../db/models')
+const {Order, User, Cart, Product} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -12,7 +12,13 @@ router.get('/', async (req, res, next) => {
     }
 
     if (currentUser.isAdmin) {
-      const orders = await Order.findAll({include: User})
+      const orders = await Order.findAll({
+        include: [
+          {
+            model: Product
+          }
+        ]
+      })
       res.json(orders)
     } else {
       res.status(401).send('Log in as an admin to view all orders.')
@@ -28,14 +34,97 @@ router.get('/me', async (req, res, next) => {
     if (req.user) {
       currentUser = req.user.dataValues
     } else {
+      currentUser = {id: 0}
+    }
+
+    if (currentUser.id) {
+      const orders = await Order.findAll({
+        where: {userId: currentUser.id},
+        include: Product
+      })
+      res.json(orders)
+    } else {
+      res.send('Log in to view your orders.')
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    let currentUser
+    if (req.user) {
+      currentUser = req.user.dataValues
+    } else {
       currentUser = {id: -1}
     }
 
-    const orders = await Order.findAll(
-      {where: {userId: currentUser.id}},
-      {include: User}
-    )
-    res.json(orders)
+    const order = await Order.findByPk(req.params.id, {include: Product})
+
+    if (currentUser.id === order.userId || currentUser.isAdmin) {
+      res.json(order)
+    } else {
+      res.send("Only admins can view other people's orders.")
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    let currentUserId
+    if (req.user) {
+      currentUserId = req.user.dataValues.id
+    } else {
+      currentUserId = 0
+    }
+
+    if (currentUserId) {
+      const productObj = {status: 'pending', userId: currentUserId}
+
+      const newOrder = await Order.create(productObj)
+      if (newOrder) {
+        res.status(201).send(newOrder)
+      } else {
+        res.status(304).send('Failed to create order.')
+      }
+    } else {
+      res.status(304).send('Log in to create an order.')
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    let currentUser
+    if (req.user) {
+      currentUser = req.user.dataValues
+    } else {
+      currentUser = {id: 1}
+    }
+
+    const order = await Order.findByPk(req.params.id)
+
+    if (currentUser.id === order.userId || currentUser.isAdmin) {
+      const {status} = req.body
+
+      const updateOrder = await Order.update(
+        {status},
+        {where: {id: req.params.id}}
+      )
+
+      if (updateOrder) {
+        res.status(202).send('Order updated!')
+      } else {
+        res.status(304).send('Failed to update order.')
+      }
+    } else {
+      res.status(304).send('Log in to update your orders.')
+    }
   } catch (err) {
     next(err)
   }
