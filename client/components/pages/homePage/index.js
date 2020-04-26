09e8e804f7356f3401cf91b28a7ee6c207d73de4
Angie Spears <angie.spears809@gmail.com>
2020-04-26 11:41:13 -0400

    add delete func and move all wines component to homePage component

--- client/components/pages/homePage/index.js
+++ client/components/pages/homePage/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchWinesFromServer} from '../../../store/allWines'
-import AllWines from '../../allWines'
+import {fetchWinesFromServer, deleteWine} from '../../../store/allWines'
+import {Link} from 'react-router-dom'
+// import AllWines from '../../allWines'
 import './style.css'
 
 class HomePage extends React.Component {
@@ -9,9 +10,29 @@ class HomePage extends React.Component {
     this.props.getAllWines()
   }
   render() {
+    const {wines} = this.props
     return (
       <div>
-        <AllWines wines={this.props.wines} />
+        <h1>ALL WINES</h1>
+        {wines
+          ? wines.map(wine => {
+              return (
+                <div className="container" key={wine.id}>
+                  <h2> {wine.name} </h2>
+                  <img src={wine.imageURL} />
+                  <h2> {wine.color} </h2>
+                  <h2> ${wine.price}</h2>
+                  <Link to={`products/${wine.id}`}>view wine</Link>
+                  <button
+                    type="button"
+                    onClick={() => this.props.handleDelete(wine.id)}
+                  >
+                    Delete product
+                  </button>
+                </div>
+              )
+            })
+          : ' '}
       </div>
     )
   }
@@ -25,7 +46,8 @@ const mapState = state => {
 
 const mapDispatch = dispatch => {
   return {
-    getAllWines: () => dispatch(fetchWinesFromServer())
+    getAllWines: () => dispatch(fetchWinesFromServer()),
+    handleDelete: wineId => dispatch(deleteWine(wineId))
   }
 }
 
--- client/store/allWines.js
+++ client/store/allWines.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const GET_WINES = 'GET_WINES'
 const ADD_NEW_WINE = 'ADD_NEW_WINE'
+const DELETE_WINE = 'DELETE_WINE'
 
 const getWines = wines => {
   return {
@@ -17,6 +18,12 @@ const setNewWine = newWine => {
   }
 }
 
+const deletedWine = wineId => {
+  return {
+    type: DELETE_WINE,
+    wineId
+  }
+}
 const initialState = {
   all: []
 }
@@ -43,12 +50,25 @@ export const addNewWine = newWineInfo => {
   }
 }
 
+export const deleteWine = wineId => {
+  return async dispatch => {
+    try {
+      const res = await axios.delete(`/api/products/${wineId}`)
+      dispatch(deletedWine(res.data))
+    } catch (err) {
+      console.log(err, 'UNABLE TO DELETE PRODUCT')
+    }
+  }
+}
 export default function allWinesReducer(state = initialState, action) {
   switch (action.type) {
     case GET_WINES:
       return {...state, all: action.wines}
     case ADD_NEW_WINE:
       return {...state, all: [...state.all, action.newWine]}
+    case DELETE_WINE:
+      const filtered = state.all.filter(wine => wine.id !== action.wineId)
+      return {...state, filtered}
     default:
       return state
   }
