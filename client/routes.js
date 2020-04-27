f5236b33f28a0f116ac8baa90e734e14143413b0
Angie Spears <angie.spears809@gmail.com>
2020-04-27 18:27:09 -0400

    update file paths

--- client/routes.js
+++ client/routes.js
@@ -4,16 +4,16 @@ import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
-import SingleProduct from './components/SingleProduct'
+import SingleProduct from './components/singleProduct'
 //import HomePage from './components/pages/HomePage'
 import Cart from './components/pages/Cart'
 
-import HomePage from './components/pages/HomePage/index.js'
+import HomePage from './components/pages/homePage/index.js'
 import AllUsers from './components/pages/allUsers/AllUsers'
 import SingleUser from './components/pages/singleUser/SingleUser'
-import RedWines from './components/pages/RedWines'
-import WhiteWines from './components/pages/WhiteWines'
-import RoseWines from './components/pages/RoseWines'
+import RedWines from './components/pages/redWines'
+import WhiteWines from './components/pages/whiteWines'
+import RoseWines from './components/pages/roseWines'
 /**
  * COMPONENT
  */
