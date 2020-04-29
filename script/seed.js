340c2ca2f89bdfb077a408c74dd86f9fdf16f8fc
Angie Spears <angie.spears809@gmail.com>
2020-04-29 10:39:38 -0400

    seeded data

--- script/seed.js
+++ script/seed.js
@@ -467,9 +467,9 @@ const products = [
 //   }
 // }
 
-// Execute the `seed` function, IF we ran this module directly (`node seed`).
-// `Async` functions always return a promise, so we can use `catch` to handle
-// any errors that might occur inside of `seed`.
+// // Execute the `seed` function, IF we ran this module directly (`node seed`).
+// // `Async` functions always return a promise, so we can use `catch` to handle
+// // any errors that might occur inside of `seed`.
 // if (module === require.main) {
 //   runSeed()
 // }
